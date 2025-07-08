5339598f157efe2af03611f4c9e91f0efe561007
Md Sujauddin Sekh <61101752+sujaudd1n@users.noreply.github.com>
2025-07-08 17:53:13 +0530

    implement login

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-from fastapi import FastAPI
+from secrets import token_urlsafe
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
+users = {
+    "sujauddin": "pssaword",
+    "akash": "bluesky",
+    "tousif": "tousifxyz"
+}
+
+login_tokens = {}
+
+class User(BaseModel):
+    username: str
+    password: str
+
 @app.post("/login/") 
-async def login():
-    return {"message": "Not implemented"}
+async def login(user: User):
+    if user.username not in users or user.password != users[user.username]:
+        return HTTPException(status_code=401, detail="Wrong username or password")
+    token = token_urlsafe(32)
+    login_tokens[token] = user.username
+    return {"token": token}
 
 @app.post("/prompt/") 
 async def prompt():
